2c5daaa8f8f0870e20f00873eb7778191e7845ed
Alex Mole <alex.molen@gmail.com>
2025-07-02 22:02:58 +0100

    measuring text & controls

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,22 @@
 #include "raylib.h"
+#include "raymath.h"
 
+#include <cassert>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
 #include <memory>
 #include <numeric>
 #include <span>
+#include <stdexcept>
 #include <sstream>
 #include <vector>
 
 using u8 = uint8_t;
+using u32 = uint32_t;
+
+
+#define FRASSERT    assert
 
 
 
@@ -20,6 +27,19 @@ inline int isize(const auto& cont)
 {
     return int(cont.size());
 }
+
+
+struct Cardinals
+{
+    float n;
+    float e;
+    float s;
+    float w;
+
+    Cardinals(float all) : n(all), e(all), s(all), w(all) { /**/ }
+    Cardinals(float ns, float ew) : n(ns), e(ew), s(ns), w(ew) { /**/ }
+    Cardinals(float n, float e, float s, float w) : n(n), e(e), s(s), w(w) { /**/ }
+};
 
 
 
@@ -55,26 +75,94 @@ namespace ui
     }
 
 
-
     struct ControlStyle
     {
+        u8 DrawBg : 1 = 1;
+
         Color NormalBg = Color { 100, 100, 98, 255 };
         Color HoverBg = LIGHTGRAY;
         Color SelectedBg = GRAY;
+
+        Color NormalFg = WHITE;
+        Color HoverFg = DARKGRAY;
+        Color SelectedFg = YELLOW;
+
+        int TextSize = kUiFontSize;
+
+        Cardinals Padding { 8.f };
     };
     const ControlStyle kDefaultControlStyle;
+    const ControlStyle kNullControlStyle = { .DrawBg = 0 };
+
 
     using ControlPtr = std::shared_ptr<struct ControlState>;
     struct ControlState
     {
-        Rectangle Rect { 0, 0, 100, 100 };
         std::string Text;
-        const ControlStyle* Style = &kDefaultControlStyle;
+        const ControlStyle* Style = &kNullControlStyle;
 
-        u8 IsHovered : 1;
-        u8 IsSelected : 1;
-        u8 IsFocused : 1;
+        std::vector<ControlPtr> Children;
+
+        Vector2 MinDesiredSize { 0.f, 0.f };
+
+        u8 IsHovered : 1 = false;
+        u8 IsSelected : 1 = false;
+        u8 IsFocused : 1 = false;
+
+        u8 DesiredSizeDirty : 1 = true;
+
+        Rectangle Rect { 0, 0, 100, 100 };
     };
+    
+    inline Color GetBgColor(const ControlState& ctrl)
+    {
+        FRASSERT(ctrl.Style);
+
+        if (ctrl.IsHovered)
+            return ctrl.Style->HoverBg;
+
+        if (ctrl.IsSelected)
+            return ctrl.Style->SelectedBg;
+
+        return ctrl.Style->NormalBg;
+    }
+    inline Color GetFgColor(const ControlState& ctrl)
+    {
+        FRASSERT(ctrl.Style);
+
+        if (ctrl.IsHovered)
+            return ctrl.Style->HoverFg;
+
+        if (ctrl.IsSelected)
+            return ctrl.Style->SelectedFg;
+
+        return ctrl.Style->NormalFg;
+    }
+
+
+    Vector2 CalcMinDesiredSize(const ControlState& ctrl)
+    {
+        constexpr float kDefaultMinDim = 8.f;
+        FRASSERT(ctrl.Style);
+
+        Vector2 minContentSize(kDefaultMinDim, kDefaultMinDim);
+
+        if (!ctrl.Text.empty())
+        {
+            const int fontSize = ctrl.Style->TextSize;
+            const Vector2 textSize = MeasureTextEx(g_fredFont, ctrl.Text.c_str(), fontSize, 0.f);
+            minContentSize = Vector2Max(minContentSize, textSize);
+        }
+
+        const Cardinals& pad = ctrl.Style->Padding;
+        return Vector2Add(minContentSize, Vector2(pad.e + pad.w, pad.n + pad.s));
+    }
+
+    void UpdateDesiredSize(ControlState& ctrl)
+    {
+        ctrl.MinDesiredSize = CalcMinDesiredSize(ctrl);
+        ctrl.DesiredSizeDirty = false;
+    }
 
 
     struct Layout_HorizBox_Params
@@ -86,7 +174,18 @@ namespace ui
 
     void Layout_HorizBox(const Rectangle& parent, std::span<ControlPtr> children, const Layout_HorizBox_Params& params)
     {
-        const float minTotalChildWidth = std::accumulate(begin(children), end(children), 0.f, [](float acc, const auto& ctrlP) { return (acc + ctrlP->Rect.width); });
+        // refresh any out of date desired sizes
+        for (ControlPtr& ctrlP : children)
+        {
+            ControlState* ctrl = ctrlP.get();
+            if (!ctrl)
+                throw std::invalid_argument("null control in Layout_HorizBox");
+
+            if (ctrl->DesiredSizeDirty)
+                UpdateDesiredSize(*ctrl);
+        }
+
+        const float minTotalChildWidth = std::accumulate(begin(children), end(children), 0.f, [](float acc, const auto& ctrlP) { return (acc + ctrlP->MinDesiredSize.x); });
         const int numChildren = isize(children);
         const float totalPadding = params.Padding * (numChildren + 1);
 
@@ -116,7 +215,7 @@ namespace ui
 
             ctrl->Rect.x = intX;
 
-            const float width = ctrl->Rect.width * scale;
+            const float width = ctrl->MinDesiredSize.x * scale;
             trueX += width;
             intX = roundf(trueX);
             ctrl->Rect.width = intX - ctrl->Rect.x;
@@ -136,17 +235,17 @@ static int gHoveredCtrlIx = -1;
 
 void initWindow()
 {
-    for (int i = 0; i < 16; ++i)
+    for (int i = 0; i < 12; ++i)
     {
         ui::ControlPtr ctrl = make_shared<ui::ControlState>();
+        ctrl->Style = &ui::kDefaultControlStyle;
 
-        std::ostringstream os;
-        os << "col " << i;
-        ctrl->Text = os.str();
+        for (int n = 0; n <= i; ++n)
+            ctrl->Text += 'x';
 
         gControls.push_back(ctrl);
     }
-    ui::Layout_HorizBox(Rectangle { 0, 200, 1280, 32 }, gControls, { .Padding=2, .Expand=true });
+    ui::Layout_HorizBox(Rectangle { 0, 200, 1600, 32 }, gControls, { .Padding=2, .Expand=true });
 }
 
 void updateHoveredCtrl(const Vector2& inputPos)
@@ -182,41 +281,36 @@ void render()
     for (const ui::ControlPtr& ctrlP : gControls)
     {
         const ui::ControlState* ctrl = ctrlP.get();
+        if (!ctrl)
+            throw std::invalid_argument("null control in render");
 
-        Color bg = ctrl->Style->NormalBg;
-        if (ctrl->IsHovered)
-            bg = ctrl->Style->HoverBg;
-        else if (ctrl->IsSelected)
-            bg = ctrl->Style->SelectedBg;
-
-
-        DrawRectangle(ctrl->Rect.x, ctrl->Rect.y, ctrl->Rect.width, ctrl->Rect.height, bg);
+        if (ctrl->Style && ctrl->Style->DrawBg)
+        {
+            const Color bgCol = ui::GetBgColor(*ctrl);
+            DrawRectangle(ctrl->Rect.x, ctrl->Rect.y, ctrl->Rect.width, ctrl->Rect.height, bgCol);
+        }
 
         if (!ctrl->Text.empty())
         {
             Vector2 cursor = { ctrl->Rect.x, ctrl->Rect.y };
-            DrawTextEx(ui::g_fredFont, ctrl->Text.c_str(), cursor, ui::kUiFontSize, 0.f, WHITE);
+            if (ctrl->Style)
+            {
+                const Cardinals pad = ctrl->Style->Padding;
+                cursor.x += pad.e;
+                cursor.y += pad.n;
+            }
+
+            const Color fgCol = ui::GetFgColor(*ctrl);
+            DrawTextEx(ui::g_fredFont, ctrl->Text.c_str(), cursor, ui::kUiFontSize, 0.f, fgCol);
         }
-    }
-
-    
-    ui::DrawUIText("oRrRange", 500, 150, ORANGE);
-
-    Vector2 cursor = { 190, 220 };
-    for (int i = 0; i < 10; ++i)
-    {
-        DrawTextEx(ui::g_fredFont, "Banana", cursor, ui::kUiFontSize, 0.f, YELLOW);
-
-        cursor.x += 80;
-        cursor.y += ui::kUiFontSize;
     }
 }
 
 
 int main(int argc, const char** argv)
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    const int screenWidth = 1600;
+    const int screenHeight = 1024;
 
     InitWindow(screenWidth, screenHeight, "fredsheet");
     ui::LoadUIFont();
